050e42f9fa7d7f39515286bbb4609f88cd8a2516
Veema Jhagru <veemajhagru@gmail.com>
2025-12-11 14:25:19 -0500

    refactored Task to use props
    Co-authored-by: Anaiah <3 <Anaiahm@users.noreply.github.com>

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,10 +1,10 @@
 import PropTypes from 'prop-types';
 import './Task.css';
 
-const Task = ({ id, title, isComplete }) => {
+const Task = (props) => {
   return (
     <li className="tasks__item">
-      <button className="tasks__item__toggle">{title}</button>
+      <button className="tasks__item__toggle">{props.title}</button>
       <button className="tasks__item__remove button">x</button>
     </li>
   );
